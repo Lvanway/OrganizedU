2d0093719499a1069163cc46442b0497bb13f279
Logan VanWay <Lvanway@outlook.com>
2023-11-30 23:23:15 -0500

    Add models project

---
+++ CC.Models/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CC.Models
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CC.Models/Students/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC.Models.Students;
+
+internal class Student
+{
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
